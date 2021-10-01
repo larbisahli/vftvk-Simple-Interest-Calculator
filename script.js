4d8447a68cd49974dec094ee21b3d2fbdfc2f896
Isaac <larbisahli1905@gmail.com>
2021-10-01 06:29:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  var princ = document.getElementById("principal");
+  var rat = document.getElementById("rate");
+  var principal = princ.value;
+  var rate = rat.value;
+  var years = document.getElementById("years").value;
+
+  if (principal <= 0) {
+    alert("Please enter the principal in positive value");
+    princ.focus();
+    return false;
+  }
+
+  if (rate <= 0) {
+    alert("Please enter the rate in positive value");
+    rat.focus();
+    return false;
+  } else {
+    var interest = (principal * years * rate) / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var result = document.getElementById("result");
+
+    result.innerHTML = `  <br />
+                          <br />If you deposit <mark>${principal}</mark>,<br />
+                          at an interest rate of <mark>${rate}</mark>,<br />
+                          You will receive an amount of <mark>${interest}</mark>,<br />
+                          in the year <mark>${year}</mark>`;
+  }
 }
-        
+function updateRate() {
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval + "%";
+}
+window.onload = () => {
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval + "%";
+};
